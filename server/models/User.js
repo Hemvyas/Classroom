57fec26f9f42bf4968b5cfebfd86ff5f71041c11
Hemvyas <hem.vyas09@gmail.com>
2024-08-10 15:50:12 +0530

    created mongoose schemas for User and Classrom

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["Principal", "Teacher", "Student"],
+    required: true,
+  },
+  classroom: { type: mongoose.Schema.Types.ObjectId, ref: "Classroom" }, // Reference to classroom for Teacher and Student
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
