2edeea97795ae9e2bcd3d050711150bb3f8f24b0
Hemvyas <hem.vyas09@gmail.com>
2024-08-12 17:55:04 +0530

    .

--- server/index.js
+++ server/index.js
@@ -3,27 +3,28 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const authRoutes=require("./routes/authRoute")
-const principalRoute=require("./routes/principalRoute")
-const studentRoute=require("./routes/studentRoute")
-const teacherRoute=require("./routes/teacherRoute")
+
+const authRoutes = require("./routes/authRoute");
+const principalRoute = require("./routes/principalRoute");
+const studentRoute = require("./routes/studentRoute");
+const teacherRoute = require("./routes/teacherRoute");
+
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+// Test route
+app.get("/api/test", (req, res) => {
+  res.send("Server is working!");
+});
 
-// //test route
-// app.get("/test", (req, res) => {
-//   res.send("Server is working!");
-// });
+app.use("/api/auth", authRoutes);
+app.use("/api/principal", principalRoute);
+app.use("/api/student", studentRoute);
+app.use("/api/teacher", teacherRoute);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/principal",principalRoute)
-app.use("/api/student",studentRoute);
-app.use("/api/teacher",teacherRoute);
+const PORT = process.env.PORT || 5000;
 
-
-const PORT=5000
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -33,6 +34,6 @@ mongoose
     console.log(err);
   });
 
-  app.listen(PORT || process.env.PORT, () => {
-    console.log("Port Started at 5000");
-  });
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
